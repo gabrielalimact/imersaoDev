67954af39acd72ef073a74c78b1f5e8f0c43a4a1
gabrielalimact <lgcl@ic.ufal.br>
2021-09-17 20:01:56 -0300

    some changes on aula1

--- aula1/script.js
+++ aula1/script.js
@@ -1,15 +1,22 @@
-var nome = "Gabriela";
-var notaPrimeiro = 9.42;
-var notaSegundo = 8.75;
-var notaTerceiro = 8.75;
-var notaQuarto = 9.777;
 
-var notaFinal = (notaPrimeiro + notaSegundo + notaTerceiro + notaQuarto)/4;
-var fixedNota = notaFinal.toFixed(2);
+function Calcular() {
+    var notaPrimeiro = parseFloat(document.getElementById("nota1").value);
+    var notaSegundo = parseFloat(document.getElementById("nota2").value);
+    var output = document.getElementById("mediaFinal");
+    var notaFinal = ((notaPrimeiro + notaSegundo)/2).toFixed(2);
+    console.log(notaPrimeiro);
+    if(notaPrimeiro > 10 || notaSegundo > 10){
+        output.innerHTML = "Você digitou uma nota acima de 10. Digite novamente!";
+    }
+    else{
+        if(notaFinal >= 7){
+        output.innerHTML = "Parabéns! Você foi aprovado com " + notaFinal + " na média.";
+        }else{
+        output.innerHTML = "Ihhh! Você foi reprovado com " + notaFinal + " na média.";
+        }
+    }
+    
+    
 
-if(fixedNota >= 7){
-    console.log(nome + " ficou com " + fixedNota + " na nota final e foi aprovado.");
-}else{
-    console.log(nome + " ficou com " + fixedNota + " na nota final e foi reprovado.");
 }
 
